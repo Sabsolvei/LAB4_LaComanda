5b886cbf719752e77ed838c2f887cd21910384e9
Sabsolvei <sabrinaveiga@hotmail.com>
2019-02-27 23:25:19 -0300

    descomento captcha

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -77,13 +77,12 @@ export class LoginComponent implements OnInit {
   }
 
   public ingresar(loginFormValues: any) {
-  //  if (this.captcha) {
-
+    if (this.captcha) {
       this._auth.loadingOn();
       this._auth.loginUser(loginFormValues.email, loginFormValues.pass)
         .catch(err => Promise.reject(err))
         .then((user: Iusuario) => {
-          this._auth.guardarPerfilNombre(user.nombre + ' (' + user.perfil + ') ' );
+          this._auth.guardarPerfilNombre(user.nombre + ' (' + user.perfil + ') ');
           if (user.perfil == 'admin') {
             this._auth.mostrarMenu();
           }
@@ -94,7 +93,7 @@ export class LoginComponent implements OnInit {
           this._auth.redireccionar(user);
           this._auth.loadingOff();
         });
-  //  }
+    }
   }
 
   public ingresarComoAnonimo() {
